797a894fef11c7cf95ce8b1345bb37510727b440
Lawrence Benson <github@lawben.com>
2022-08-26 15:16:08 +0200

    Add intial idea for benchmarks

---
+++ hash_bucket.cpp
@@ -0,0 +1,168 @@
+#include <array>
+#include <cstdint>
+#include <numeric>
+
+#include "benchmark/benchmark.h"
+
+struct Entry {
+  uint64_t key;
+  uint64_t value;
+};
+
+struct alignas(256) HashBucket {
+  alignas(256) std::array<uint8_t, 15> fingerprints;
+  alignas(16) std::array<Entry, 15> entries;
+};
+
+static constexpr uint64_t NO_MATCH = std::numeric_limits<uint64_t>::max();
+
+static_assert(sizeof(HashBucket) == 256, "Hash Bucket should be 256 Byte for this benchmark");
+
+#define BM_ARGS UseRealTime()->Repetitions(10);
+
+template <typename FindFn>
+void BM_hash_bucket_get(benchmark::State& state) {
+  FindFn find_fn{};
+  HashBucket bucket{};
+  bucket.fingerprints = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+  bucket.entries = {Entry{11, 11},   Entry{12, 12},   Entry{33, 33},   Entry{44, 44},   Entry{55, 55},
+                    Entry{66, 66},   Entry{77, 77},   Entry{88, 88},   Entry{99, 99},   Entry{100, 100},
+                    Entry{110, 110}, Entry{120, 120}, Entry{130, 130}, Entry{140, 140}, Entry{150, 150}};
+  uint64_t key = 66;
+  uint8_t fingerprint = 6;
+
+  for (auto _ : state) {
+    uint64_t value = find_fn(bucket, key, fingerprint);
+    benchmark::DoNotOptimize(value);
+  }
+}
+
+#if defined(__aarch64__)
+#include <arm_neon.h>
+
+struct neon_find {
+  uint64_t operator()(HashBucket& bucket, uint64_t key, uint8_t fingerprint) {
+    uint8_t* fingerprints = bucket.fingerprints.data();
+
+    // Load the fingerprints into a SIMD register.
+    uint8x16_t fp_vector = vld1q_u8(fingerprints);
+
+    // Broadcast the fingerprint to compare against into a SIMD register.
+    uint8x16_t lookup_fp = vmovq_n_u8(fingerprint);
+
+    // Compare fingerprints.
+    auto matching_fingerprints = reinterpret_cast<__uint128_t>(vceqq_u8(fp_vector, lookup_fp));
+
+    // We could do this with a single movemask on x86, but ARM NEON does not support this. So we split our range into
+    // two values that we check after each other. The extraction here is a no-op, as the __uint128_t result is stored in
+    // two 64 bit registers anyway. This is only a logical conversion.
+    uint64_t low_matches = *reinterpret_cast<uint64_t*>(&matching_fingerprints);
+    uint64_t high_matches = *(reinterpret_cast<uint64_t*>(&matching_fingerprints) + 1);
+
+    // We need to pass an offset here, as the higher matches need to check the second half of the entries array.
+    auto find_key_match = [&](uint64_t matches, size_t entry_offset) {
+      while (matches != 0) {
+        // The comparison returns 00000000 for a mismatch, so we need to divide by 8 to get the actual number of 0's.
+        uint32_t trailing_zeros = __builtin_ctzl(matches);
+        uint16_t match_pos = entry_offset + (trailing_zeros / 8);
+
+        // We give this a likely hint, as we expect the number of fingerprint collisions to be low. So on average, we
+        // want this to be the happy path and immediately return if possible.
+        if (bucket.entries[match_pos].key == key) [[likely]] {
+          return bucket.entries[match_pos].value;
+        }
+
+        // We want to remove all 1's that we just matched. So we set all 8 bits that we just matched and invert the
+        // number for a clean 11111111...00000000...11111111 mask.
+        matches &= ~(255ul << trailing_zeros);
+      }
+      return NO_MATCH;
+    };
+
+    uint64_t low_match = find_key_match(low_matches, 0);
+    if (low_match != NO_MATCH) {
+      return low_match;
+    }
+
+    return find_key_match(high_matches, 8);
+  }
+};
+
+BENCHMARK(BM_hash_bucket_get<neon_find>)->BM_ARGS;
+
+#elif defined(__x86_64__)
+struct x86_find {
+  uint64_t operator()(HashBucket& bucket, uint64_t key, uint8_t fingerprint) {
+    // TODO
+    uint8_t* fingerprints = bucket.fingerprints.data();
+    return 0;
+  }
+};
+
+BENCHMARK(BM_hash_bucket_get<x86Find>)
+    ->BM_ARGS;
+#endif
+
+struct naive_scalar_find {
+  uint64_t operator()(HashBucket& bucket, uint64_t key, uint8_t fingerprint) { return NO_MATCH; }
+};
+
+struct autovec_scalar_find {
+  uint64_t operator()(HashBucket& bucket, uint64_t key, uint8_t fingerprint) { return NO_MATCH; }
+};
+
+struct vector_find {
+  using vec8x16 = uint8_t __attribute__((vector_size(16)));
+  uint64_t operator()(HashBucket& bucket, uint64_t key, uint8_t fingerprint) {
+    uint8_t* fingerprints = bucket.fingerprints.data();
+
+    // Load the fingerprints into a SIMD register.
+    vec8x16 fp_vector = *reinterpret_cast<vec8x16*>(fingerprints);
+
+    // Broadcast the fingerprint to compare against into a SIMD register. We only use 15 values, to the last one is 0.
+    uint8_t f = fingerprint;
+    vec8x16 lookup_fp = {f, f, f, f, f, f, f, f, f, f, f, f, f, f, f, 0};
+
+    // Compare fingerprints.
+    auto matching_fingerprints = reinterpret_cast<__uint128_t>(fp_vector == lookup_fp);
+
+    // We could do this with a single movemask on x86, but ARM NEON does not support this. So we split our range into
+    // two values that we check after each other. The extraction here is a no-op, as the __uint128_t result is stored
+    // in two 64 bit registers anyway. This is only a logical conversion.
+    uint64_t low_matches = *reinterpret_cast<uint64_t*>(&matching_fingerprints);
+    uint64_t high_matches = *(reinterpret_cast<uint64_t*>(&matching_fingerprints) + 1);
+
+    // We need to pass an offset here, as the higher matches need to check the second half of the entries array.
+    auto find_key_match = [&](uint64_t matches, size_t entry_offset) {
+      while (matches != 0) {
+        // The comparison returns 00000000 for a mismatch, so we need to divide by 8 to get the actual number of 0's.
+        uint32_t trailing_zeros = __builtin_ctzl(matches);
+        uint16_t match_pos = entry_offset + (trailing_zeros / 8);
+
+        // We give this a likely hint, as we expect the number of fingerprint collisions to be low. So on average, we
+        // want this to be the happy path and immediately return if possible.
+        if (bucket.entries[match_pos].key == key) [[likely]] {
+          return bucket.entries[match_pos].value;
+        }
+
+        // We want to remove all 1's that we just matched. So we set all 8 bits that we just matched and invert the
+        // number for a clean 11111111...00000000...11111111 mask.
+        matches &= ~(255ul << trailing_zeros);
+      }
+      return NO_MATCH;
+    };
+
+    uint64_t low_match = find_key_match(low_matches, 0);
+    if (low_match != NO_MATCH) {
+      return low_match;
+    }
+
+    return find_key_match(high_matches, 8);
+  }
+};
+
+BENCHMARK(BM_hash_bucket_get<naive_scalar_find>)->BM_ARGS;
+BENCHMARK(BM_hash_bucket_get<autovec_scalar_find>)->BM_ARGS;
+BENCHMARK(BM_hash_bucket_get<vector_find>)->BM_ARGS;
+
+BENCHMARK_MAIN();
