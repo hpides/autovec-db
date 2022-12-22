44cdedaff2c0e5d731ca4ddc234837ed2c1fb6b8
Richard Ebeling <He3lixxx@users.noreply.github.com>
2022-12-22 11:30:31 +0100

    Hashing: Consistent variable naming (shift / bits_required), prevent return value copying

--- hashing.cpp
+++ hashing.cpp
@@ -22,40 +22,36 @@ using AlignedArray = BaseAlignedArray<uint64_t, NUM_KEYS>;
 // Constant taken from https://github.com/rurban/smhasher
 constexpr static uint64_t MULTIPLY_CONSTANT = 0x75f17d6b3588f843ull;
 
-/** We assume a basic multiply-shift hash, based on the constants used in SMHasher and 27 Bits needed in the hash table.
- * We add this range to avoid the compiler optimizing the subtraction here. We perform a multiplication followed by a
- * right-shift by (64 - shift). */
-uint64_t calculate_hash(uint64_t key, uint64_t shift) { return (key * MULTIPLY_CONSTANT) >> (64 - shift); }
+uint64_t calculate_hash(uint64_t key, uint64_t required_bits) {
+  return (key * MULTIPLY_CONSTANT) >> (64 - required_bits);
+}
 
 template <typename HashFn>
 void BM_hashing(benchmark::State& state) {
   HashFn hash_fn{};
-  uint64_t shift = state.range(0);
+  uint64_t required_bits = state.range(0);
 
   // Seed rng for same benchmark runs.
   std::mt19937_64 rng{345873456};
 
   AlignedArray keys_to_hash;
-  AlignedArray check_keys;
+  AlignedArray correct_hash_values;
   for (size_t i = 0; i < NUM_KEYS; ++i) {
     keys_to_hash[i] = rng();
-    check_keys[i] = calculate_hash(keys_to_hash[i], shift);
+    correct_hash_values[i] = calculate_hash(keys_to_hash[i], required_bits);
   }
 
   AlignedArray hashes{};
-  // Fill with 0s here as we will most likely reuse the same memory after each run, and we use uninitialized array to
-  // avoid zeroing explicitly in each iteration.
-  std::fill(hashes.begin(), hashes.end(), 0);
 
   // Do one sanity check that we get the correct results.
-  hashes = hash_fn(keys_to_hash, shift);
-  if (hashes != check_keys) {
+  hash_fn(keys_to_hash, required_bits, &hashes);
+  if (hashes != correct_hash_values) {
     throw std::runtime_error{"Bad hash calculation"};
   }
 
   for (auto _ : state) {
     benchmark::DoNotOptimize(keys_to_hash.data());
-    hashes = hash_fn(keys_to_hash, shift);
+    hash_fn(keys_to_hash, required_bits, &hashes);
     benchmark::DoNotOptimize(hashes);
   }
 }
@@ -65,16 +61,16 @@ void BM_hashing(benchmark::State& state) {
 
 /** Doing this in NEON is not very useful, as we can neither do a vector multiply nor a variable right shift. */
 struct neon_hash {
-  AlignedArray operator()(const AlignedArray& keys_to_hash, uint64_t shift) {
+  void operator()(const AlignedArray& keys_to_hash, uint64_t required_bits, AlignedArray* __restrict result) {
+    static_assert(NUM_KEYS % 2 == 0);
     using VecArray = std::array<uint64x2_t, NUM_KEYS / 2>;
 
-    AlignedArray actual_hashes;
-    auto& hashes = reinterpret_cast<VecArray&>(actual_hashes);
+    auto& hashes = reinterpret_cast<VecArray&>(*result);
 
     // We need this "hack" here, as NEON requires the right shift value to be a compile-time constant. So we shift left
     // with a negative value.
-    int64_t actual_shift = -(64 - static_cast<int64_t>(shift));
-    uint64x2_t shift_value = vmovq_n_u64(actual_shift);
+    int64_t shift_by = -(64 - static_cast<int64_t>(required_bits));
+    uint64x2_t shift_value = vmovq_n_u64(shift_by);
 
     for (size_t i = 0; i < NUM_KEYS / 2; ++i) {
       const size_t offset = i * 2;
@@ -83,27 +79,24 @@ struct neon_hash {
       uint64x2_t multiplied_keys = vld1q_u64(keys.data());
       hashes[i] = vshlq_u64(multiplied_keys, shift_value);
     }
-
-    return actual_hashes;
   }
 };
 
 #elif defined(__x86_64__)
 struct x86_128_hash {
-  AlignedArray operator()(const AlignedArray& keys_to_hash, uint64_t shift) {
+  void operator()(const AlignedArray& keys_to_hash, uint64_t required_bits, AlignedArray* __restrict result) {
     // TODO
     (void)keys_to_hash;
-    (void)shift;
-    return AlignedArray{};
+    (void)required_bits;
+    (void)result;
   }
 };
 BENCHMARK(BM_hashing<x86_128_hash>)->BM_ARGS;
 
 #if defined(AVX512_AVAILABLE)
 struct x86_512_hash {
-  AlignedArray operator()(const AlignedArray& keys_to_hash, uint64_t shift) {
+  void operator()(const AlignedArray& keys_to_hash, uint64_t required_bits, AlignedArray* __restrict result) {
     // TODO
-    return AlignedArray{};
   }
 };
 BENCHMARK(BM_hashing<x86_512_hash>)->BM_ARGS;
@@ -112,28 +105,24 @@ BENCHMARK(BM_hashing<x86_512_hash>)->BM_ARGS;
 #endif
 
 struct naive_scalar_hash {
-  AlignedArray operator()(const AlignedArray& keys_to_hash, uint64_t shift) {
-    AlignedArray hashes;
+  void operator()(const AlignedArray& keys_to_hash, uint64_t required_bits, AlignedArray* __restrict result) {
     for (size_t i = 0; i < NUM_KEYS; ++i) {
-      hashes[i] = calculate_hash(keys_to_hash[i], shift);
+      (*result)[i] = calculate_hash(keys_to_hash[i], required_bits);
     }
-    return hashes;
   }
 };
 
 struct autovec_scalar_hash {
-  AlignedArray operator()(const AlignedArray& keys_to_hash, uint64_t shift) {
-    AlignedArray hashes;
+  void operator()(const AlignedArray& keys_to_hash, uint64_t required_bits, AlignedArray* __restrict result) {
+    // TODO(Richard): Actually, the naive code is already perfectly vectorizable. Check out generated assembly.
     for (size_t i = 0; i < NUM_KEYS; ++i) {
-      hashes[i] = keys_to_hash[i] * MULTIPLY_CONSTANT;
+      (*result)[i] = keys_to_hash[i] * MULTIPLY_CONSTANT;
     }
 
-    uint64_t actual_shift = 64 - shift;
+    uint64_t shift = 64 - required_bits;
     for (size_t i = 0; i < NUM_KEYS; ++i) {
-      hashes[i] = hashes[i] >> actual_shift;
+      (*result)[i] = (*result)[i] >> shift;
     }
-
-    return hashes;
   }
 };
 
@@ -147,18 +136,15 @@ struct vector_hash {
 
   using VecArray = std::array<VecT, NUM_KEYS / NUM_VECTOR_ELEMENTS>;
 
-  AlignedArray operator()(const AlignedArray& keys_to_hash, uint64_t shift) {
+  void operator()(const AlignedArray& keys_to_hash, uint64_t required_bits, AlignedArray* __restrict result) {
     const auto& vec_keys = reinterpret_cast<const VecArray&>(keys_to_hash);
 
-    AlignedArray actual_hashes;
-    auto& hashes = reinterpret_cast<VecArray&>(actual_hashes);
+    auto& hashes = reinterpret_cast<VecArray&>(*result);
 
-    uint64_t actual_shift = 64 - shift;
+    uint64_t shift = 64 - required_bits;
     for (size_t i = 0; i < NUM_KEYS / NUM_VECTOR_ELEMENTS; ++i) {
-      hashes[i] = (vec_keys[i] * MULTIPLY_CONSTANT) >> actual_shift;
+      hashes[i] = (vec_keys[i] * MULTIPLY_CONSTANT) >> shift;
     }
-
-    return actual_hashes;
   }
 };
 
