1f8d8b3c33717e6954f57a11c9b7e1d81a2a21d3
Lawrence Benson <github@lawben.com>
2022-12-23 11:29:27 +0100

    Try fix

---
+++ scripts/format.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." >/dev/null 2>&1 && pwd )"
+
+function run_format() {
+    local subdir=$1
+    find "${DIR}"/"${subdir}" \( -iname '*.hpp' -o -iname '*.cpp' \) -print0 | xargs -0 clang-format -i
+}
+
+run_format "include"
+run_format "src"
+run_format "test"
+run_format "examples"
+run_format "benchmarks"
