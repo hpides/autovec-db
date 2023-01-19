89bbe14e8369c696297375b43d03cfaa1ab1053a
Lawrence Benson <github@lawben.com>
2023-01-19 10:24:08 +0100

    Add initial plot frame (#14)

---
+++ eval/scripts/common.py
@@ -0,0 +1,148 @@
+import sys
+import os
+import matplotlib
+import matplotlib.pyplot as plt
+
+import pandas as pd
+pd.options.mode.chained_assignment = None  # default='warn'
+
+
+#######################################
+# Plotting
+#######################################
+
+FONT_SIZE = 20
+MILLION = 1_000_000
+SINGLE_FIG_WIDTH = 5
+SINGLE_FIG_HEIGHT = 3.5
+SINGLE_FIG_SIZE = (SINGLE_FIG_WIDTH, SINGLE_FIG_HEIGHT)
+DOUBLE_FIG_WIDTH = 10
+DOUBLE_FIG_HEIGHT = 3.5
+DOUBLE_FIG_SIZE = (DOUBLE_FIG_WIDTH, DOUBLE_FIG_HEIGHT)
+IMG_TYPES = ('.png', '.svg')
+
+
+INTEL_BLUE = '#0071c5'
+APPLE_GREY = '#555555'
+
+
+VARIANT_COLOR_BLACK_WHITE = {
+    "scalar": '#f0f0f0',
+    "autovec": '#bdbdbd',
+    "vec": '#737373',
+    "x86": '#252525',
+    "neon": '#252525',
+}
+
+VARIANT_COLOR = {
+    "scalar": '#ffffb2',
+    "autovec": '#fecc5c',
+    "vec": '#fd8d3c',
+    "x86": '#e31a1c',
+    "neon": '#e31a1c',
+}
+
+def get_color(variant_name, use_black_white=False):
+    colors = VARIANT_COLOR_BLACK_WHITE if use_black_white else VARIANT_COLOR
+    for name, color in colors.items():
+        if name in variant_name:
+            return color
+    raise RuntimeError(f"No color found for variant: {variant_name}")
+
+
+def INIT_PLOT():
+    matplotlib.rcParams.update({
+        'font.size': FONT_SIZE,
+        'svg.fonttype': 'none',
+    })
+
+
+def INIT(args):
+    if len(args) != 3:
+        sys.exit("Need /path/to/results /path/to/plots")
+
+    result_path = args[1]
+    plot_dir = args[2]
+
+    os.makedirs(plot_dir, exist_ok=True)
+    INIT_PLOT()
+
+    return result_path, plot_dir
+
+
+def BAR(variant):
+    return {
+        "color": get_color(variant),
+        "edgecolor": 'black',
+        "width": 0.7,
+        "lw": 2
+    }
+
+
+def RESIZE_TICKS(ax, x=FONT_SIZE, y=FONT_SIZE):
+    for tick in ax.xaxis.get_major_ticks():
+        tick.label.set_fontsize(x)
+    for tick in ax.yaxis.get_major_ticks():
+        tick.label.set_fontsize(y)
+
+
+def HATCH_WIDTH(width=4):
+    matplotlib.rcParams['hatch.linewidth'] = width
+
+
+def Y_GRID(ax):
+    ax.grid(axis='y', which='major')
+    ax.set_axisbelow(True)
+
+
+def HIDE_BORDERS(ax, show_left=False):
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.spines['bottom'].set_visible(True)
+    ax.spines['left'].set_visible(show_left)
+
+
+def FIG_LEGEND(fig):
+    fig.legend(loc='upper center', bbox_to_anchor=(0.5, 1.1), ncol=6,
+               frameon=False, columnspacing=1, handletextpad=0.3
+               #, borderpad=0.1, labelspacing=0.1, handlelength=1.8
+              )
+    fig.tight_layout()
+
+
+def SAVE_PLOT(plot_path, img_types=IMG_TYPES):
+    plot_paths = []
+    for img_type in img_types:
+        img_path = f"{plot_path}{img_type}"
+        plot_paths.append(img_path)
+        plt.savefig(img_path, bbox_inches='tight', dpi=300)
+
+    plt.figure()
+    print(f"To view new plots, run:\n\topen {' '.join(plot_paths)}")
+
+
+#######################################
+# Benchmark Results
+#######################################
+
+def get_results(result_dir, file_name, columns=('name', 'cpu_time')):
+    df = pd.read_csv(f"{result_dir}/{file_name}")
+    df = df[[col for col in columns]]
+    df = df.rename(columns={"cpu_time": "runtime"})
+    return df
+
+
+def clean_up_results(results):
+    results = results[results.name.str.contains("mean")]
+
+    # Generic BM_... regex replace
+    results.name = results.name.replace({r"BM_.*?<(.*)>/.*" : r'\1'}, regex=True)
+
+    results.name = results.name.str.replace("_hash", "")
+    results.name = results.name.str.replace("<", "-")
+    results.name = results.name.str.replace(">", "")
+    results.name = results.name.str.replace("naive_", "")
+    results.name = results.name.str.replace("autovec_scalar", "autovec")
+    results.name = results.name.str.replace("_", "-")
+    results.name = results.name.str.replace("vector", "vec")
+    return results
---
+++ eval/scripts/hashing.py
@@ -0,0 +1,48 @@
+import sys
+import os
+sys.path.append(os.path.dirname(sys.path[0]))
+from common import *
+
+
+def plot_hashing(ax, data):
+    for _, row in data.iterrows():
+        variant = row['name']
+        ax.bar(variant, row['runtime'], **BAR(variant))
+
+    ax.set_xticks([x - 0.2 for x in range(len(data['name']))], data['name'], rotation=75, ha='center')
+    ax.tick_params(axis='x', which=u'both',length=0)
+    ax.set_xticklabels(data['name'], rotation=75)
+
+
+if __name__ == '__main__':
+    result_path, plot_dir = INIT(sys.argv)
+
+    x86_results = get_results(result_path, "hashing_x86.csv")
+    x86_results = clean_up_results(x86_results)
+
+    m1_results = get_results(result_path, "hashing_m1.csv")
+    m1_results = clean_up_results(m1_results)
+
+    fig, (x86_ax, m1_ax) = plt.subplots(1, 2, figsize=DOUBLE_FIG_SIZE)
+
+    plot_hashing(x86_ax, x86_results)
+    plot_hashing(m1_ax, m1_results)
+
+    x86_ax.set_title("a) x86")
+    m1_ax.set_title("b) M1")
+
+    x86_ax.set_ylabel("Runtime (ns)")
+
+    x86_ax.set_ylim(0, 55)
+    x86_ax.set_yticks(range(0, 55, 20))
+
+    m1_ax.set_ylim(0, 30)
+    m1_ax.set_yticks(range(0, 30, 10))
+
+    HATCH_WIDTH()
+    for ax in (x86_ax, m1_ax):
+        Y_GRID(ax)
+        HIDE_BORDERS(ax)
+
+    plot_path = os.path.join(plot_dir, "hashing")
+    SAVE_PLOT(plot_path)
