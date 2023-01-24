649aa3313ebf36012ac5ddf70a590578d40f7678
Richard Ebeling <He3lixxx@users.noreply.github.com>
2023-01-24 14:48:08 +0100

    PR review

--- scripts/docker_compile_and_run.sh
+++ scripts/docker_compile_and_run.sh
@@ -5,13 +5,15 @@
 # For an interactive shell, use:
 # docker run -it -v "$(pwd):/autovec-db" autovec-db /bin/bash
 
-docker run -it -v "$(pwd):/autovec-db" autovec-db mkdir -p /autovec-db/build-clang-release
-docker run -it -v "$(pwd):/autovec-db" -e CXX=clang++-15 autovec-db cmake -S /autovec-db -B /autovec-db/build-clang-release -DCMAKE_BUILD_TYPE=Release
-docker run -it -v "$(pwd):/autovec-db" autovec-db cmake --build /autovec-db/build-clang-release -j
+BUILD_DIR="/autovec-db/build-clang-release"
+
+docker run -it -v "$(pwd):/autovec-db" autovec-db mkdir -p ${BUILD_DIR}
+docker run -it -v "$(pwd):/autovec-db" -e CXX=clang++-15 autovec-db cmake -S /autovec-db -B ${BUILD_DIR} -DCMAKE_BUILD_TYPE=Release
+docker run -it -v "$(pwd):/autovec-db" autovec-db cmake --build ${BUILD_DIR} -j
 
 BENCH_ARGS="--benchmark_format=csv --benchmark_repetitions=10 --benchmark_report_aggregates_only=true"
 
-docker run -it -v "$(pwd):/autovec-db" autovec-db /bin/bash -c "cd /autovec-db; ./build-clang-release/hashing ${BENCH_ARGS} | tee hashing.csv"
-docker run -it -v "$(pwd):/autovec-db" autovec-db /bin/bash -c "cd /autovec-db; ./build-clang-release/hash_bucket ${BENCH_ARGS} | tee hash_bucket.csv"
-docker run -it -v "$(pwd):/autovec-db" autovec-db /bin/bash -c "cd /autovec-db; ./build-clang-release/compressed_scan ${BENCH_ARGS} | tee compressed_scan.csv"
-docker run -it -v "$(pwd):/autovec-db" autovec-db /bin/bash -c "cd /autovec-db; ./build-clang-release/compare_to_bitmask ${BENCH_ARGS} | tee compare_to_bitmask.csv"
+docker run -it -v "$(pwd):/autovec-db" --workdir ${BUILD_DIR} autovec-db /bin/bash -c "./hashing ${BENCH_ARGS} | tee hashing.csv"
+docker run -it -v "$(pwd):/autovec-db" --workdir ${BUILD_DIR} autovec-db /bin/bash -c "./hash_bucket ${BENCH_ARGS} | tee hash_bucket.csv"
+docker run -it -v "$(pwd):/autovec-db" --workdir ${BUILD_DIR} autovec-db /bin/bash -c "./compressed_scan ${BENCH_ARGS} | tee compressed_scan.csv"
+docker run -it -v "$(pwd):/autovec-db" --workdir ${BUILD_DIR} autovec-db /bin/bash -c "./compare_to_bitmask ${BENCH_ARGS} | tee compare_to_bitmask.csv"
