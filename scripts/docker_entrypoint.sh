cf71e3e3f12cd5b40e15e910887681a2790b7f4d
Richard Ebeling <He3lixxx@users.noreply.github.com>
2023-06-07 11:45:37 +0200

    fixup! Go back to GCC 12

--- scripts/docker_entrypoint.sh
+++ scripts/docker_entrypoint.sh
@@ -1,7 +1,7 @@
 BUILD_DIR=./build-gcc-release
 BENCH_ARGS="--benchmark_format=csv --benchmark_repetitions=10 --benchmark_report_aggregates_only=true"
 
-COMPILER="g++-13"
+COMPILER="g++-12"
 
 mkdir -p ${BUILD_DIR}
 CXX=${COMPILER} cmake . -B ${BUILD_DIR} -DCMAKE_BUILD_TYPE=Release
