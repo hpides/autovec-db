3423e40de188d410d7a7f23966d1ddc0f5e5b1f9
Lawrence Benson <github@lawben.com>
2023-06-09 17:09:19 +0200

    Add new docker entry script

--- scripts/docker_entrypoint.sh
+++ scripts/docker_entrypoint.sh
@@ -1,12 +1,20 @@
-BUILD_DIR=./build-gcc-release
 BENCH_ARGS="--benchmark_format=csv --benchmark_repetitions=10 --benchmark_report_aggregates_only=true"
 
-COMPILER="g++-12"
+LLVM_COMPILER="${AUTOVEC_DB_COMPILER:-clang++}"
+GCC_COMPILER="g++-12"
 
-mkdir -p ${BUILD_DIR}
-CXX=${COMPILER} cmake . -B ${BUILD_DIR} -DCMAKE_BUILD_TYPE=Release
-cmake --build ${BUILD_DIR} -j
+run_benchmarks () {
+  COMPILER=$1
+  NAME=$2
+  BUILD_DIR="./build-${NAME}-release"
+  mkdir -p ${BUILD_DIR}
+  CXX=${COMPILER} cmake . -B ${BUILD_DIR} -DCMAKE_BUILD_TYPE=Release
+  cmake --build ${BUILD_DIR} -j
 
-for BENCHMARK in hashing hash_bucket compressed_scan dictionary_scan; do
-  ${BUILD_DIR}/${BENCHMARK} ${BENCH_ARGS} | tee ${BENCHMARK}.csv
-done
+  for BENCHMARK in hashing hash_bucket compressed_scan dictionary_scan; do
+    ${BUILD_DIR}/${BENCHMARK} ${BENCH_ARGS} | tee ${BENCHMARK}.csv
+  done
+}
+
+run_benchmarks llvm ${LLVM_COMPILER}
+run_benchmarks gcc ${GCC_COMPILER}
